7d247a381835196af59c1193745ffb72d37eedbc
joaquindiazs <joaquindiazschillagi@gmail.com>
2020-03-17 12:08:28 +0100

    Install babel-core & loader package. add babel to webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,40 @@
 module.exports = {
-    entry: './app/index.js',
+    entry: "./app/index.js",
     output: {
-        filename: 'bundle.js',
+        filename: "bundle.js",
         path: __dirname
     },
-    devtool: 'eval-source-map',
+    devtool: "eval-source-map",
     module: {
         loaders: [
             {
-            	test:/\.scss$/, 
-            	loader:'style-loader!css-loader!sass-loader', 
-            	exclude: /node_modules/
+                test: /\.scss$/,
+                loader: "style-loader!css-loader!sass-loader",
+                exclude: /node_modules/
+            }
+        ]
+    }
+};
+
+module.exports = {
+    entry: "./app/index.js",
+    output: {
+        filename: "bundle.js",
+        path: __dirname
+    },
+    devtool: "eval-source-map",
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+                query: { presets: ["es2015"] }
+            },
+            {
+                test: /\.scss$/,
+                loader: "style-loader!css-loader!sass-loader",
+                exclude: /node_modules/
             }
         ]
     }
