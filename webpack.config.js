491dd2576966afe221c8a902a4e622eececb9bb1
joaquindiazs <joaquindiazschillagi@gmail.com>
2020-03-17 11:50:50 +0100

    Install loaders packages for webpack & install compiler node-sass.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,16 @@
 module.exports = {
-	entry: "./app/index.js",
-	output: {
-		filename: "bundle.js",
-		path: __dirname
-	}
+    entry: './app/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: __dirname
+    },
+    module: {
+        loaders: [
+            {
+            	test:/\.scss$/, 
+            	loader:'style-loader!css-loader!sass-loader', 
+            	exclude: /node_modules/
+            }
+        ]
+    }
 };
