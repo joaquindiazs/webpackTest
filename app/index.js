2abd451b361da84c59a620bbd886224f9ea8a794
joaquindiazs <joaquindiazschillagi@gmail.com>
2020-03-17 11:51:39 +0100

    Rename extension css -> .scss. Remove css reference from index.html and import inside index.js

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
+//import Logger
 var Logger = require('./logger.js');
 
+//import CSS
+require('../styles.scss');
+
 //update content of greeting element when DOM has been loaded
-(function(){
-    document.addEventListener('DOMContentLoaded', function(){
-        document.getElementById('greeting').innerHTML = "Hello my friend!";
-        Logger("Greeting has been updated");
-    })
-})();
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('greeting').innerHTML = "Hello my friend!";
+    Logger("Greeting has been updated");
+})
